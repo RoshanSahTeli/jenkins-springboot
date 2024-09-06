49d05cb9035a1ac363f0e8b3ee9488c853629dfa
roshan <roshanshah920@gmail.com>
2024-09-06 14:29:28 +0545

    first commit

---
+++ src/main/java/com/example/demo/JenkinsSpringbootApplication.java
@@ -0,0 +1,14 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JenkinsSpringbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JenkinsSpringbootApplication.class, args);
+		System.out.println("Hello it is running on jenkins");
+	}
+
+}
