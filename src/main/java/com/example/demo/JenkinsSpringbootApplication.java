acbb05576ff9d09f779b21505e6062e752ec124e
roshan <roshanshah920@gmail.com>
2024-09-07 13:54:19 +0545

    test

--- src/main/java/com/example/demo/JenkinsSpringbootApplication.java
+++ src/main/java/com/example/demo/JenkinsSpringbootApplication.java
@@ -9,6 +9,7 @@ public class JenkinsSpringbootApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(JenkinsSpringbootApplication.class, args);
 		System.out.println("Hello it is running on jenkins");
+		System.out.println("Automatic build");
 	}
 
 }
